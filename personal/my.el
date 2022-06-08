dca1757813336f8eef43f8a1974d2a9fe6f991f5
hwa <dreameration@gmail.com>
2022-06-08 15:52:25 +0800

    intially customiz my own config

---
+++ personal/my.el
@@ -0,0 +1,11 @@
+;; (setq ivy-re-builders-alist
+;;       '((swiper . ivy--regex-plus)
+;;         (t . ivy--regex-fuzzy)))
+
+;;(setq markdown-command "markdown -f fencedcode,toc,urlencodedanchor,header -S -T")
+(setq markdown-command "~/bin/mymarkdown.sh")
+
+(setq ivy-use-selectable-prompt t)
+
+(add-to-list 'default-frame-alist
+             '(vertical-scroll-bars . nil))
